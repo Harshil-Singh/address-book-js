4cdc6478d680d5ff2c9ee1e62c8ede5e4af832f4
Your Name <harshil0412singh@gmail.com>
2025-03-03 16:09:48 +0530

    Harshil add . Added ability to find and delete contact by name

--- Address_book_JS/src/index.js
+++ Address_book_JS/src/index.js
@@ -5,24 +5,26 @@ const addressBook = new AddressBook();
 
 try {
     const contact1 = new Contact(
-        "Harshil", "S", "Bhopal", "India", "In", "100001", "9876543210", "Harshil@gmail.com"
+        "harshil", "Soni", "Bhopal", "India", "In", "100001", "9876543210", "harshilsoni9165@gmail.com"
     );
     console.log(addressBook.addContact(contact1)); 
 
     const contact2 = new Contact(
-        "Rakesh", "Soni", "Bhopal", "India","In","1000000","9197979988","rakeshsoni395@gmail.com"
+        "jitendra", "Soni", "Bhopal", "India","In","1000000","9197979988","jitendrasoni395@gmail.com"
     )
     console.log(addressBook.addContact(contact2)); 
     console.log("All Contacts Before editing", addressBook.getAllContacts());
-    console.log(addressBook.editContact("Rakesh", { address: "Pipariya", city: "New York", phone: "9234567890" }));
+    console.log(addressBook.editContact("jitendra", { address: "Pipariya", city: "New York", phone: "9234567890" }));
     console.log("All Contacts After editing", addressBook.getAllContacts());
+    console.log(addressBook.deleteContact("jitendra"));
+    console.log("All Contacts After Deleting:", addressBook.getAllContacts());
 } catch (error) {
     console.error("Error:", error.message);
 }
 
 try {
     const invalidContact = new Contact(
-        "an", "sn", "abc", "In", "I", "100A01", "12345", "invalid-email"
+        "an", "son", "abc", "In", "I", "100A01", "12345", "invalid-email"
     );
     console.log(addressBook.addContact(invalidContact));
 } catch (error) {
